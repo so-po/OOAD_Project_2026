edde1a82a7895f3e036f97cd86bc3b3de07d7e91
MelanieXinyi <melanie.xinyi.porter@gmail.com>
2026-04-21 15:47:42 -0600

    Grid builder patter

--- src/conwaygame/Game/ConwayGame.java
+++ src/conwaygame/Game/ConwayGame.java
@@ -4,7 +4,9 @@ public class ConwayGame { //Facade for Game MVC
 
     public ConwayGame(int width, int height) throws Exception {
         GameViewer gameViewer = new GameViewer(width, height);
-        Grid grid = new Grid(width, height);
+
+        Grid grid = new Grid.GridBuilder().setDimensions(width, height).addDefaultCreatureToLocation(0,0)
+                .addDefaultCreatureToLocation(1,0).addDefaultCreatureToLocation(1,1).build();
         GameController game = new GameController(grid, gameViewer);
 
         gameViewer.setVisible(true);
--- src/conwaygame/Game/Grid.java
+++ src/conwaygame/Game/Grid.java
@@ -5,6 +5,7 @@ import conwaygame.creatures.CreatureType;
 import conwaygame.creatures.StrategyFactory;
 
 import java.awt.*;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
@@ -17,6 +18,43 @@ public class Grid {
     Creature[][] cells;
     StrategyFactory strategyFactory = new StrategyFactory();
     Random random;
+
+    //used for injecting creatures
+    public static class SpawnEntry{
+        String type;
+        int x;
+        int y;
+        public SpawnEntry(String type, int x, int y){
+            this.type = type;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public Grid(int width, int height, ArrayList<SpawnEntry> spawnCreatureLocations) throws Exception {
+        if (width <= 0 || height <= 0) {
+            throw new Exception("Bad grid width & height input.");
+        }
+
+        GRID_COLUMNS = width;
+        GRID_ROWS = height;
+
+        random = new Random();
+        cells = new Creature[GRID_ROWS][GRID_COLUMNS];
+
+        for (Creature[] row : cells) { //initialize list to be full of dead cells
+            for (int i = 0; i < GRID_COLUMNS; i++) {
+                row[i] = new Creature();
+            }
+        }
+
+        //spawn based on injected creatures
+        for (SpawnEntry creature : spawnCreatureLocations){
+            toggleCellState(creature.x, creature.y, creature.type);
+//            getCell(creature.x,creature.y);
+            reviveCell(creature.x,creature.y);
+        }
+    }
 
     public Grid(int width, int height) throws Exception {
         if (width <= 0 || height <= 0) {
@@ -36,6 +74,44 @@ public class Grid {
         }
     }
 
+    public static GridBuilder getNewBuilder() {
+        return new GridBuilder();
+    }
+
+    public static class GridBuilder {
+        int gridWidth;
+        int gridHeight;
+        ArrayList<SpawnEntry> spawnCreatureLocations = new ArrayList<>();
+
+        public GridBuilder() {}
+
+        public GridBuilder setDimensions(int x, int y) {
+            this.gridWidth = x;
+            this.gridHeight = y;
+            return this;
+        }
+
+        public GridBuilder addDefaultCreatureToLocation(int x, int y){
+            spawnCreatureLocations.add(new SpawnEntry("DEFAULT", x, y));
+            return this;
+        }
+
+        public GridBuilder addExplosiveCreatureToLocation(int x, int y){
+            spawnCreatureLocations.add(new SpawnEntry("EXPLOSIVE", x, y));
+            return this;
+        }
+
+        public GridBuilder addScarcityCreatureToLocation(int x, int y){
+            spawnCreatureLocations.add(new SpawnEntry("SCARCITY", x, y));
+            return this;
+        }
+
+        public Grid build() throws Exception {
+            return new Grid(gridWidth, gridHeight, spawnCreatureLocations);
+            //TODO: if dimensions not set, make dimensions based on the existing cells
+        }
+    }
+
     private Creature getCell(int x, int y) { return cells[y][x]; }
 
     private boolean cellExists(int x, int y) {
@@ -47,7 +123,6 @@ public class Grid {
             Creature cell = getCell(x, y);
             cell.makeAlive();
         }
-
     }
 
     public void toggleCellState(int x, int y, String selectedType) {
